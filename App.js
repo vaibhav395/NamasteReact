abd14225556b3241ad7a8b90d3715d04dd186afb
vaibhav395 <90473158+vaibhav395@users.noreply.github.com>
2024-02-27 17:03:00 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+// 1- Creating a single element
+// const element = React.createElement("h1", {id : "heading"}, "Hello World from React!");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(element);
+
+
+// 2 - Creating nested elements and siblings
+{/* <div id="parent">
+    <div id="child">
+        <h1>I am Main heading</h1>
+        <h2>I am Secondary heading</h2>
+    </div>
+</div> */}
+
+const parent = React.createElement("div", {id:"parent"}, [React.createElement("div", {id:"child"}, 
+[React.createElement("h1", {}, "I am main heading"), React.createElement("h2", {},"I am Secondary heading")]),
+
+React.createElement("div", {id:"child2"}, 
+[React.createElement("h1", {}, "I am main heading2"), React.createElement("h2", {},"I am Secondary heading2")])]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
